fad680e2179748c195ad993fd6e7835f0e547cb2
Mackenzie Grimes - NOAA Affiliate <mackenzie.grimes@noaa.gov>
2024-12-18 09:46:54 -0700

    delete unnecessary private methods

--- python/idsse/testing/ims_service/src/profile_store.py
+++ python/idsse/testing/ims_service/src/profile_store.py
@@ -33,10 +33,7 @@ class ProfileStore:
                 os.mkdir(_dir)
 
         # cache of JSON data of all Support Profiles, divided into new vs. existing Profiles
-        self.new_profiles = []
-        self.existing_profiles = []
-
-        logger.info('Loading existing Support Profiles from path: %s', self._existing_dir)
+        self.new_profiles: list[dict] = []
         self.existing_profiles = self._read_existing_profiles()
 
     def get_all(self, filter_new_profiles = False) -> list[dict]:
@@ -60,8 +57,17 @@ class ProfileStore:
             str | None: UUID of saved Support Profile on success, otherwise None
         """
         logger.debug('Now saving new profile: %s', profile)
-        # save to JSON file and add to in-memory cache
-        return self._save_profile(profile, is_new=True)
+        profile_id = profile.get('id')
+
+        # determine the right filepath where JSON data will be written
+        filepath = os.path.join(self._new_dir , profile_id + '.json')
+
+        logger.info('Now saving profile to path: %s', filepath)
+        with open(filepath, 'w', encoding='utf-8') as file:
+            json.dump(profile, file)  # save JSON to filesystem
+
+        self.new_profiles.append(profile)  # add profile to in-memory cache
+        return profile_id
 
     def move_to_existing(self, profile_id: str) -> bool:
         """Mark a formerly "new" Support Profile as "existing", a.k.a. has been returned in
@@ -90,10 +96,13 @@ class ProfileStore:
                            profile_id)
 
             # unexpectedly, profile is not in new_profiles cache;
-            # recover from this by re-reading the JSON from file, because file did exist
-            profile_data = self._read_existing_profile(profile_id)
+            # recover from this by re-reading the JSON from file, because file must exist
+            filename = os.path.join(self._existing_dir, profile_id + '.json')
+            logger.debug('Attempting to read existing profile from path: %s', filename)
+            with open(filename, 'r', encoding='utf-8') as file:
+                profile_data = json.load(file)
 
-        # add Profile/Profile to existing_profiles cache, and scrub from new_profiles cache
+        # add Profile to existing_profiles cache, and scrub from new_profiles cache
         self.existing_profiles.append(profile_data)
         self.new_profiles = [profile for profile in self.existing_profiles
                              if profile['id'] != profile_id]
@@ -106,49 +115,32 @@ class ProfileStore:
         Returns:
             bool: True on success
         """
-        logger.debug('Attempting to delete profile_id %s', profile_id)
-        is_deleted = self._delete_profile(profile_id)
-        if not is_deleted:
-            return False
+        logger.info('Deleting profile_id %s', profile_id)
+        filepath = os.path.join(self._existing_dir, profile_id + '.json')
+        profile_cache = self.existing_profiles  # assume profile is in "existing" cache
 
-        # drop profile from new or existing cache (could be in either)
-        self.new_profiles = [profile for profile in self.new_profiles
-                             if profile['id'] != profile_id]
-        self.existing_profiles = [profile for profile in self.existing_profiles
-                                  if profile['id'] != profile_id]
+        if not os.path.exists(filepath):
+            # profile does not in exist in "existing" subdirectory, maybe its in "new"
+            filepath = os.path.join(self._existing_dir, profile_id + '.json')
+            profile_cache = self.new_profiles  # now assume profile is in "new" cache
+
+            if not os.path.exists(filepath):
+                logger.warning('Cannot delete profile %s; JSON file not found in %s or %s',
+                               profile_id, self._existing_dir, self._new_dir)
+                return False
+
+        logger.debug('Attempting to delete profile at path: %s', filepath)
+        os.remove(filepath)
+
+        # drop profile from new/existing cache (whichever subdirectory it was found in)
+        profile_cache = [profile for profile in profile_cache if profile['id'] != profile_id]
 
         return True
 
-    # private methods that do the actual disk read/write operations
-    def _save_profile(self, profile: dict, is_new: bool) -> str:
-        """Writes profile JSON to disk and adds the JSON to appropriate in-memory cache:
-        `self.new_profiles` if `is_new`, otherwise `self.existing_profiles`.
-
-        Args:
-            profile (dict): full JSON data to be saved
-            is_new (bool): if True, profile will be saved in subdirectory and in-memory cache for
-                "new" profiles. Otherwise will be stored in "existing" profiles subdir/cache.
-        Returns:
-            str: the profile_id, if save was successful
-        """
-        profile_id = profile.get('id')
-
-        # determine the right filepath where JSON data will be written
-        dir_path = self._new_dir if is_new else self._existing_dir
-        filepath = os.path.join(dir_path, profile_id + '.json')
-
-        # will be saved to the appropriate in-memory list of profile data
-        profile_cache = self.new_profiles if is_new else self.existing_profiles
-
-        logger.info('Now saving profile to path: %s', filepath)
-        with open(filepath, 'w', encoding='utf-8') as file:
-            json.dump(profile, file)  # save JSON to filesystem
-
-        profile_cache.append(profile)
-        return profile_id
-
     def _read_existing_profiles(self) -> list[dict]:
         """Read all JSON files from this ProfileStore's `existing` subdirectory"""
+        logger.info('Loading existing Support Profiles from path: %s', self._existing_dir)
+
         profile_list: list[dict] = []
         for filename in glob('*.json', root_dir=self._existing_dir):
             with open(os.path.join(self._existing_dir, filename), 'r', encoding='utf-8') as file:
@@ -162,37 +154,3 @@ class ProfileStore:
                     profile_list.append(json_data)
 
         return profile_list
-
-    def _read_existing_profile(self, profile_id: str) -> dict:
-        """Get an existing profile from disk based on the profile ID.
-        Reads from NEW subdirectory should almost never be necessary.
-        """
-        filename = os.path.join(self._existing_dir, profile_id + '.json')
-        logger.debug('Attempting to read existing profile from path: %s', filename)
-        with open(filename, 'r', encoding='utf-8') as file:
-            return json.load(file)
-
-    def _write_new_profile(self, data: dict):
-        """Write a new Support Profile Profile to disk using `self._new_dir`"""
-        profile_id = data['id']
-        with open(os.path.join(self._new_dir, profile_id + '.json'), 'w', encoding='utf-8') as file:
-            os.write(file, data)
-
-    def _delete_profile(self, profile_id: str) -> bool:
-        """Delete profile from disk by profile_id.
-        Returns:
-            bool: True on success, False if JSON file not found
-        """
-        filepath = os.path.join(self._existing_dir, profile_id + '.json')
-        if not os.path.exists(filepath):
-            # profile does not in exist in existing subdirectory, maybe its in the new one
-            filepath = os.path.join(self._existing_dir, profile_id + '.json')
-
-            if not os.path.exists(filepath):
-                logger.warning('Cannot delete profile %s; JSON file not found in %s or %s',
-                               profile_id, self._existing_dir, self._new_dir)
-                return False
-
-        logger.debug('Attempting to delete profile at path: %s', filepath)
-        os.remove(filepath)
-        return True
