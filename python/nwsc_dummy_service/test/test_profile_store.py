e79aa312c26df105f7142cd1b3531d3897292470
Mackenzie Grimes - NOAA Affiliate <mackenzie.grimes@noaa.gov>
2024-12-18 16:32:37 -0700

    improve test coverage, fix unit test import

--- python/nwsc_dummy_service/test/test_profile_store.py
+++ python/nwsc_dummy_service/test/test_profile_store.py
@@ -15,10 +15,9 @@ import shutil
 from copy import deepcopy
 from glob import glob
 
-from pytest import fail, fixture
+from pytest import fixture
 
-from nwsc_dummy_service.src.profile_store import (CachedProfile, ProfileStore,
-                                                  NEW_SUBDIR, EXISTING_SUBDIR)
+from python.nwsc_dummy_service.src.profile_store import ProfileStore, NEW_SUBDIR, EXISTING_SUBDIR
 
 # constants
 STORE_BASE_DIR = os.path.join(os.path.dirname(__file__), 'temp')
@@ -27,7 +26,7 @@ RAW_JSON_PATH = os.path.join(os.path.dirname(__file__), '..', 'src', 'profiles')
 EXAMPLE_UUID = '9835b194-74de-4321-aa6b-d769972dc7cb'
 
 with open(os.path.join(RAW_JSON_PATH, 'nwsc_test_response_1.json'), 'r', encoding='utf-8') as f:
-    EXAMPLE_SUPPORT_PROFILE: dict = json.load(f)
+    EXAMPLE_SUPPORT_PROFILE: dict = json.load(f)['profiles'][0]
 
 
 def _empty_directory(dir_path: str):
@@ -69,6 +68,7 @@ def startup_and_teardown():
 def store():
     return ProfileStore(STORE_BASE_DIR)
 
+
 # tests
 def test_profile_store_loads_api_responses(store: ProfileStore):
     assert sorted([c.id for c in store.profile_cache]) == [
@@ -85,6 +85,21 @@ def test_profile_store_loads_api_responses(store: ProfileStore):
 
     # new directory should be empty to begin with
     assert os.listdir(os.path.join(STORE_BASE_DIR, NEW_SUBDIR)) == []
+
+
+def test_store_loads_jsons_from_new(store: ProfileStore):
+    # create a pre-existing "new" profile as well as the 3 "existing" profiles
+    profile = deepcopy(store.get_all()[0])
+    profile['id'] = EXAMPLE_UUID  # give copied profile a unique identifier
+    store.save(profile)
+
+    # simulate starting ProfileStore process fresh, with existing JSONs on filesystem
+    _new_store = ProfileStore(STORE_BASE_DIR)
+
+    # newly creatd ProfileStore should have correctly loaded and labeled "new" Profile
+    new_profile_list = _new_store.get_all(filter_new_profiles=True)
+    assert len(new_profile_list) == 1
+    assert len(_new_store.profile_cache) == 4  # 3 existing, 1 new
 
 
 def test_get_all_profiles(store: ProfileStore):
@@ -114,6 +129,19 @@ def test_save_adds_to_new_profiles(store: ProfileStore):
     assert os.path.exists(os.path.join(STORE_BASE_DIR, NEW_SUBDIR, f'{new_profile_id}.json'))
 
 
+def test_save_rejects_existing_profile(store: ProfileStore):
+    new_profile = deepcopy(EXAMPLE_SUPPORT_PROFILE)  # use Support Profile that already exists
+
+    new_profile_id = store.save(new_profile)
+
+    assert not new_profile_id
+    # no new profile should have been added
+    new_profile_list = store.get_all(filter_new_profiles=True)
+    assert new_profile_list == []
+    # file should not exist in the "new" subdirectory
+    assert not os.path.exists(os.path.join(STORE_BASE_DIR, NEW_SUBDIR, f'{new_profile["id"]}.json'))
+
+
 def test_move_to_existing_success(store: ProfileStore):
     new_profile = deepcopy(EXAMPLE_SUPPORT_PROFILE)
     new_profile['id'] = EXAMPLE_UUID
@@ -130,16 +158,21 @@ def test_move_to_existing_success(store: ProfileStore):
     assert EXAMPLE_UUID in [p['id'] for p in existing_profiles]  # now Profile is in existing list
 
 
-def test_store_loads_jsons_from_new(store: ProfileStore):
-    # create a pre-existing "new" profile as well as the 3 "existing" profiles
-    profile = deepcopy(store.get_all()[0])
-    profile['id'] = EXAMPLE_UUID  # give copied profile a unique identifier
-    store.save(profile)
+def test_delete_profile(store: ProfileStore):
+    existing_profile_list = store.get_all()
+    profile_id = existing_profile_list[0]['id']
 
-    # simulate starting ProfileStore process fresh, with existing JSONs on filesystem
-    _new_store = ProfileStore(STORE_BASE_DIR)
+    success = store.delete(profile_id)
 
-    # newly creatd ProfileStore should have correctly loaded and labeled "new" Profile
-    new_profile_list = _new_store.get_all(filter_new_profiles=True)
-    assert len(new_profile_list) == 1
-    assert len(_new_store.profile_cache) == 4  # 3 existing, 1 new
+    # after delete, profile should not be returned to get() request, and JSON file should be gone
+    assert success
+    existing_profile_list = store.get_all()
+    assert profile_id not in [p['id'] for p in existing_profile_list]
+    assert not os.path.exists(os.path.join(STORE_BASE_DIR, EXISTING_SUBDIR, f'{profile_id}.json'))
+
+
+def test_delete_profile_failure(store: ProfileStore):
+    profile_id = '11111111-2222-3333-444444444444'  # fake ID does not exist in ProfileStore
+
+    success = store.delete(profile_id)
+    assert not success
