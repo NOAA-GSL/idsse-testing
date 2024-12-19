889e9e42c8efd973f46339bb476c15f6a5231b9d
Mackenzie Grimes - NOAA Affiliate <mackenzie.grimes@noaa.gov>
2024-12-19 12:16:42 -0700

    allow dataSource 'ANY'

--- python/nwsc_dummy_service/ncd_web_service.py
+++ python/nwsc_dummy_service/ncd_web_service.py
@@ -75,7 +75,7 @@ class EventsRoute:
 
         # otherwise, must be 'GET' operation
         data_source = request.args.get('dataSource', None, type=str)
-        if data_source != 'NBM':
+        if data_source not in ['NBM', 'ANY']:
             return jsonify({'profiles': [], 'errors': [f'Invalid dataSource: {data_source}']}), 400
 
         profile_status = request.args.get('status', default='existing', type=str)
