e4449ad39e43ec305d8fbef5a3f66ccbf0f230b9
Mackenzie Grimes - NOAA Affiliate <mackenzie.grimes@noaa.gov>
2025-05-02 09:50:44 -0600

    reformat all Python files with black

--- python/nwsc_proxy/test/__init__.py
+++ python/nwsc_proxy/test/__init__.py
@@ -1,4 +1,6 @@
 """Add nwsc_proxy top-level module to sys.path"""
+
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,5 @@
 """Setup to support installation as Python library"""
+
 import os
 from setuptools import setup
 
@@ -6,53 +7,65 @@ from setuptools import setup
 def package_files(directory):
     """Discover all files in a given package directory"""
     paths: list[str] = []
-    for (path, directories, filenames) in os.walk(directory):
+    for path, directories, filenames in os.walk(directory):
         for filename in filenames:
             paths.append(os.path.join(path, filename))
     return paths
 
 
-setup(name='idsse-testing',
-      version='1.0',
-      description='IDSSe Common Test Data and External Proxies',
-      url='',
-      author='WIDS',
-      author_email='@noaa.gov',
-      license='MIT',
-      python_requires=">=3.11",
-      packages=['idsse.testing.data_access',
-                'idsse.testing.data_access.data_cache',
-                'idsse.testing.data_access.mrms_nssl_grib',
-                'idsse.testing.data_access.nbm_aws_grib',
-                'idsse.testing.data_access.validation',
-                'idsse.testing.event_portfolios',
-                'idsse.testing.idsse_common',
-                'idsse.testing.nwsc_gateway',
-                'idsse.testing.risk_processor',
-                'idsse.testing.risk_processor.binghamton',
-                'idsse.testing.risk_processor.i87',
-                'idsse.testing.risk_processor.simple',
-                'idsse.testing.risk_processor.syracuse',
-                'idsse.testing.risk_processor.percentiles',
-                'idsse.testing.utils'],
-      data_files=[('idsse.testing.idsse_common', package_files('idsse/testing/idsse_common/')),
-                  ('idsse.testing.data_access.data_cache', package_files('idsse/testing/data_access/data_cache')),
-                  ('idsse.testing.data_access.mrms_nssl_grib', package_files('idsse/testing/data_access/mrms_nssl_grib')),
-                  ('idsse.testing.data_access.nbm_aws_grib', package_files('idsse/testing/data_access/nbm_aws_grib')),
-                  ('idsse.testing.data_access.validation', package_files('idsse/testing/data_access/validation'))],
-      include_package_data=True,
-      package_data={'': ['*.csv', '*.json', '*.nc', '*.grib2*']},
-      install_requires=[
-        'pika',
-        'jsonschema',
-        'netcdf4',
-        'h5netcdf',
-        'python-logging-rabbitmq'
-      ],
-      extras_require={
-        'develop': [
-          'pytest',
-          'pytest-cov',
+setup(
+    name="idsse-testing",
+    version="1.0",
+    description="IDSSe Common Test Data and External Proxies",
+    url="",
+    author="WIDS",
+    author_email="@noaa.gov",
+    license="MIT",
+    python_requires=">=3.11",
+    packages=[
+        "idsse.testing.data_access",
+        "idsse.testing.data_access.data_cache",
+        "idsse.testing.data_access.mrms_nssl_grib",
+        "idsse.testing.data_access.nbm_aws_grib",
+        "idsse.testing.data_access.validation",
+        "idsse.testing.event_portfolios",
+        "idsse.testing.idsse_common",
+        "idsse.testing.nwsc_gateway",
+        "idsse.testing.risk_processor",
+        "idsse.testing.risk_processor.binghamton",
+        "idsse.testing.risk_processor.i87",
+        "idsse.testing.risk_processor.simple",
+        "idsse.testing.risk_processor.syracuse",
+        "idsse.testing.risk_processor.percentiles",
+        "idsse.testing.utils",
+    ],
+    data_files=[
+        ("idsse.testing.idsse_common", package_files("idsse/testing/idsse_common/")),
+        (
+            "idsse.testing.data_access.data_cache",
+            package_files("idsse/testing/data_access/data_cache"),
+        ),
+        (
+            "idsse.testing.data_access.mrms_nssl_grib",
+            package_files("idsse/testing/data_access/mrms_nssl_grib"),
+        ),
+        (
+            "idsse.testing.data_access.nbm_aws_grib",
+            package_files("idsse/testing/data_access/nbm_aws_grib"),
+        ),
+        (
+            "idsse.testing.data_access.validation",
+            package_files("idsse/testing/data_access/validation"),
+        ),
+    ],
+    include_package_data=True,
+    package_data={"": ["*.csv", "*.json", "*.nc", "*.grib2*"]},
+    install_requires=["pika", "jsonschema", "netcdf4", "h5netcdf", "python-logging-rabbitmq"],
+    extras_require={
+        "develop": [
+            "pytest",
+            "pytest-cov",
         ]
-      },
-      zip_safe=False)
+    },
+    zip_safe=False,
+)
